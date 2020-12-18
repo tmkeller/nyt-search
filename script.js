e171e0319cc933179401955c3de71105886cfb01
Tim Keller <timothy.m.keller@gmail.com>
2020-12-18 10:58:28 -0800

    Page space.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var APIkey = "WrB1zbsUayijQqUnzNy7tinYsH6U1Tp4";
 var AppID = "42cb5969-6f50-4bdd-8ddb-1d207e52757e";
 
+
 var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${ APIkey }`;
 
 $.ajax({
