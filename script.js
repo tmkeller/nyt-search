922210cf1c590e6165a18cd8a7cdcac4efb6baa4
Tim Keller <timothy.m.keller@gmail.com>
2020-12-18 11:27:50 -0800

    Added console test cases.

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ var endYear = $("#endYear").val()
 
 var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${ APIkey }`;
 
+var response;
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then( function( response ) {
     console.log( response );
+    // console.log( response.response );
+    // console.log( response.response.docs );
+    // var docs = response.response.docs;
+    // for ( var i = 0; i < docs.length; i++ ) {
+    //     console.log( docs[ i ].headline.main );
+    //     console.log( docs[ i ].abstract );
+    //     console.log( docs[ i ].web_url );
+    // }
 })
